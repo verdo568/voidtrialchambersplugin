5c0f63b65b60db5c2b9f05ab1f9c4149e89b1a54
swim5678 <154345558+verdo568@users.noreply.github.com>
2025-04-30 23:42:27 +0800

    更新「審判」難度邏輯：每五波刷怪時施加負面效果並生成審判首領

--- src/main/java/org/swim/voidTrialChambersPlugin/VoidTrialChambersPlugin.java
+++ src/main/java/org/swim/voidTrialChambersPlugin/VoidTrialChambersPlugin.java
@@ -1178,7 +1178,7 @@ public class VoidTrialChambersPlugin extends JavaPlugin implements Listener {
 
             // Track wave count for JUDGMENT difficulty
             waveCount++;
-            if (diff == TrialDifficulty.JUDGMENT && waveCount % 10 == 0) {
+            if (diff == TrialDifficulty.JUDGMENT && waveCount % 5 == 0) {
                 applyNegativeEffectsToSurvival();
                 spawnJudgmentBoss();
             }
