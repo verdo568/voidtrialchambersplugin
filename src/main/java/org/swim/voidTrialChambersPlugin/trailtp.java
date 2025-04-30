e0f17e521880e61e3e773b40deb1edb131b045c9
swim5678 <154345558+verdo568@users.noreply.github.com>
2025-04-30 11:38:13 +0800

    移除未使用的 `ChatColor` 匯入以簡化代碼

--- src/main/java/org/swim/voidTrialChambersPlugin/trailtp.java
+++ src/main/java/org/swim/voidTrialChambersPlugin/trailtp.java
@@ -1,7 +1,6 @@
 package org.swim.voidTrialChambersPlugin;
 
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
